e94456498806da70275ffd5ea0d4fc908ccf8a3d
Kevin Bennett <kevinfpbennett@gmail.com>
2020-07-01 10:23:28 -0400

    Added zoomed plots with gene bars above

--- scripts/plot.popgen.R
+++ scripts/plot.popgen.R
@@ -2,10 +2,140 @@
 # ---------------------------------- Set up -----------------------------------
 # =============================================================================*
 
-# Change path if you are not Kevin
-setwd('C:/Users/kbenn/Documents/grad/phd/dissertation/data/pigment/')
+# Set directory and load packages ------------------------------------------
+
 library(tidyverse)
 library(ggplot2)
+library(grid)
+library(gridExtra)
+library(gtable)
+
+
+# Functions ----------------------------------------------------------------
+
+make.bottom <- function(plotPars){
+  stats_data <- get(plotPars[[1]])
+  scaf <- plotPars[[2]]
+  x <- plotPars[[3]]
+  y <- plotPars[[4]]
+  xlims <- plotPars[[5]]
+  xbreaks <- plotPars[[6]]
+  xlabs <- plotPars[[7]]
+  ylims <- plotPars[[8]]
+  ybreaks <- plotPars[[9]]
+  ylabs <- plotPars[[10]]
+  
+  gg.bottom <- 
+    ggplot(data = data[data$scaffold == scaf,], aes(x = get(x), y = get(y)))+
+    geom_point()+
+    scale_x_continuous(
+      breaks = xbreaks, labels = xlabs, limits = xlims, expand = c(0, 0))+
+    scale_y_continuous(
+      breaks = ybreaks, labels = ylabs, limits = ylims, expand = c(0, 0))+
+    theme_bw()+
+    theme(legend.position = 'none', 
+          panel.background = element_rect(fill = 'gray97'),
+          panel.border = element_rect(color = 'gray97'),
+          panel.grid = element_blank(),
+          axis.title.y = element_blank(),
+          axis.text.y = element_text(size = 14),
+          axis.title.x = element_blank(),
+          axis.line.y = element_blank())
+  return(gg.bottom)
+}
+
+prep.gene.data <- function(gplot, anno, ggene = NULL){
+  scaf <- as.character(gplot$data$scaffold[1])
+  loc <- gplot$scales$scales[[1]]$limits
+  genes <- anno %>%
+    filter(feature == 'gene', scaffold == scaf, start > loc[1],end < loc[2]) %>%
+    pull(gene)
+  
+  exongff <- anno[anno$gene %in% genes & anno$feature == 'exon',] %>%
+    mutate(isGene = gene == ggene)
+  
+  isFirstTscpt <- vector()
+  for(i in 1:length(genes)){
+    isFirstTscpt <-
+      c(isFirstTscpt,
+        exongff[exongff$gene == genes[i],]$transcript_id ==
+          exongff[exongff$gene == genes[i],]$transcript_id[1]
+      )}
+  exongff <- cbind(exongff, isFirstTscpt) %>%
+    filter(isFirstTscpt == TRUE)
+  
+  return(exongff)
+}
+
+make.top <- function(dat, plotPars){
+  glabel <- plotPars[[11]]
+  xlims <- plotPars[[5]]
+  ggene <- dat[dat$isGene == TRUE,]$gene %>% unique
+  
+  glstart <- c()
+  glend <- c()
+  glgene <- c()
+  for(i in 1:length(unique(dat$gene))){
+    newdf <- dat[dat$gene == unique(dat$gene)[i],]
+    glstart[i] <- min(newdf$start)
+    glend[i] <- max(newdf$end)
+    glgene[i] <- newdf$gene %>% unique
+  }
+  glines <- data.frame(start = glstart, end = glend, glgene = glgene) %>%
+    mutate(isGene = glgene == ggene)
+  
+  gg.top <- ggplot(data = dat)+
+    geom_segment(data = glines, 
+                 aes(x = start, xend = end, y = -4, yend = -4,
+                     color = ifelse(isGene == TRUE, 'red', 'gray50')))+
+    geom_rect(aes(xmin = start, xmax = end, ymin = -15, ymax = 9,
+                  fill = ifelse(isGene == TRUE, 'red', 'gray50')),
+              color = ifelse(dat$isGene == TRUE, 'red', 'gray50'),
+              size = 0.6)+
+    scale_fill_manual(values = c('gray50', 'red'))+
+    scale_color_manual(values = c('gray50', 'red'))+
+    scale_y_continuous(limits = c(-15, 30))+
+    scale_x_continuous(limits = xlims)+
+    theme_bw()+
+    theme(legend.position = 'none',
+          panel.grid = element_blank(),
+          panel.border = element_blank(),
+          axis.title.y = element_blank(),
+          axis.text.y = element_blank(),
+          axis.title.x = element_blank(),
+          axis.text.x = element_blank(),
+          axis.line.y = element_blank(),
+          axis.ticks = element_blank())
+  if(glabel == TRUE){
+    gg.top <- gg.top+
+      geom_text(mapping = aes(x = dat[dat$isGene == TRUE,]$start[1], y = 22), 
+                label = ggene, color = 'red', size = 3.5, nudge_x = 8000)}
+  return(gg.top)
+}
+
+plot.gene.stats <- function(
+  annotation, gene, 
+  stats_data, scaf, x, y,
+  xlims, xbreaks = NULL, xlabs = NULL, 
+  ylims, ybreaks = NULL, ylabs = NULL, 
+  glabel = FALSE
+){
+  plotPars <- list(stats_data, scaf, x, y, xlims, xbreaks, xlabs, 
+                   ylims, ybreaks, ylabs, glabel)
+  bottom <- make.bottom(plotPars)
+  exongff <- prep.gene.data(bottom, annotation, gene)
+  top <- make.top(exongff, plotPars)
+  
+  gtop <- ggplotGrob(top)
+  gbot <- ggplotGrob(bottom)
+  
+  g <- rbind(gtop, gbot, size = 'last')
+  panels <- g$layout$t[grepl('panel', g$layout$name)]
+  g$heights[panels] <- unit(c(0.85,1.5), 'cm')
+  
+  grid.newpage()
+  grid.draw(g)
+}
 
 
 # =============================================================================*
@@ -22,12 +152,17 @@ pg.c <- read.csv('popgen_min21_10k.csv') %>%
 pg.c <- read.csv('popgen_min21_25k.csv') %>%
   drop_na()
 
-# Input for 50kb windows
-pg.c <- read.csv('popgen_chrom_50k.csv') %>%
-  drop_na()
+# Read in GFF modified to be R-friendly, keep important info
+gff <- fread('../genomes/Mvitellinus/Mvit_gff_simplified.txt', sep = '\t') %>%
+  separate(col = V9, into = c('other','gene'), sep = 'gene=', remove = FALSE) %>%
+  select(scaffold = V1, feature = V3, start = V4, end = V5, gene, V9) %>%
+  separate(col = gene, into = c('gene', 'remove1'), sep = ';') %>%
+  separate(col = V9,into = c('remove2','transcript'),sep='transcript_id=') %>%
+  separate(col = transcript, into = c('transcript_id', 'remove3'), sep = ';') %>%
+  select(-c(remove1, remove2, remove3))
 
 
-# Plot ------------------------------------------------------------------------
+# Whole-geome plots ----------------------------------------------------------
 
 # Plotting using ggplot --------
 
@@ -139,29 +274,38 @@ plot(pg.c[pg.c$sites > 45,]$plotpos,
      col = fstcols[as.numeric(pg.c[pg.c$sites > 45,]$chrom)], 
      pch = 20)
 
-plot(pg.c[pg.c$sites > 5,]$pos, 
-     pg.c[pg.c$sites > 5,]$pi_4, 
-     col = picols[as.numeric(pg.c[pg.c$sites > 5,]$chrom)], 
-     pch = 20)
 
-plot(pg.c$pos, 
-     pg.c$dxy_3_4, 
-     col = dxycols[as.numeric(pg.c$chrom)], 
-     pch = 20)
+# Zoomed plots showing genes above -------------------------------------------
 
-plot(pg.c$pos, 
-     pg.c$fd, 
-     col = fdcols[as.numeric(pg.c$chrom)], 
-     pch = 20)
+# Fst plot with BCO2 highlighted and labeled
+plot.gene.stats(
+  annotation = gff,
+  gene = 'BCO2',
+  stats_data = 'pg.c',
+  scaf = 'NW_021940545.1',
+  x = 'mid',
+  y = 'Fst_3_4',
+  xlims = c(5300000, 5900000),
+  xbreaks = seq(from = 5400000, to = 5800000, by = 200000),
+  xlabs = c('5.4 Mb', '5.6 Mb', '5.8 Mb'),
+  ylims = c(-0.01, 0.55),
+  ybreaks = c(0, 0.5),
+  ylabs = c('0', '0.5'),
+  glabel = TRUE
+)
 
-plot(pg.c$pos, 
-     pg.c$freq, 
-     ylim = c(0, 0.4),
-     col = freqcols[as.numeric(pg.c$chrom)], 
-     pch = 20)
+# Fst plot with RSPO2 highlighted but not labeled, no ticks on X-axis
+plot.gene.stats(
+  annotation = gff,
+  gene = 'RSPO2',
+  stats_data = 'pg.c',
+  scaf = 'NW_021939396.1',
+  x = 'mid',
+  y = 'fd',
+  xlims = c(16850000, 17550000),
+  ylims = c(-0.1, 1.05),
+  ybreaks = c(0, 1),
+  ylabs = c('0', '1')
+)
 
-plot(pg.c$pos, 
-     pg.c$pi_3, 
-     ylim = c(0, 0.6),
-     col = picols[as.numeric(pg.c$chrom)], 
-     pch = 20)
+
